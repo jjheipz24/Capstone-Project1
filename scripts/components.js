52f2cf24d6518f996f5cec1bfbb433db49ad8ac7
Jin Jin Heipler <heipler.jj@gmail.com>
2020-09-30 13:50:53 -0400

    Created and added component for portfolios

--- scripts/components.js
+++ scripts/components.js
@@ -1,6 +1,31 @@
 //Template for components
 
-Vue.component('name', {
-    props: [],
-    template: `<div> </div>`
+Vue.component('portfolio-slat', {
+    props: ['pfp', 'major', 'name', 'blurb', 'chips', 'photo1', 'photo2'],
+    template: `<div class="portfolio>
+                <b-row>
+                    <b-col>
+                        <div class="pfp_holder">
+                            <b-img v-bind:src="pfp" alt="Profile Picture"></b-img>
+                        </div>
+                    </b-col>
+                    <b-col>
+                        <div class="info">
+                            <span class="major">{{ major }}</span>
+                            <h2 class="name">{{ name }}</h2>
+                            <p class="bio">{{ blurb }}</p>
+                        </div>
+                    </b-col>
+                    <b-col>
+                        <div class="photo-holder>
+                            <b-img thumbnail fluid v-bind:src="photo1" alt="First project image"</b-img>
+                        </div>
+                    </b-col>
+                    <b-col>
+                    <div class="photo-holder>
+                        <b-img thumbnail fluid v-bind:src="photo2" alt="Second project image"</b-img>
+                    </div>
+                </b-col>
+                </b-row>
+            </div>`
 });
