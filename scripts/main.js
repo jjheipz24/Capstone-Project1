599a839c8bdb7b2b925dc60328d6aedb118c1372
Jin Jin Heipler <heipler.jj@gmail.com>
2020-09-30 13:00:09 -0400

    setup grid and started adding in photos. created json object

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,16 @@ let app = new Vue({
     el: '#root',
     data: {
         test: "testing vue and sass",
+        portfolios: [{
+            pfp:'',
+            major: '4 year New Media Design (BFA)',
+            name: 'Sarah Danseglio',
+            blurb: "I aim to improve experiences through the effects of visuals and aesthetics. I'm a hard worker and a good listener, and I have a little too much patience when it comes to tedious tasks.",
+            chips: ["Visual Design", "Interactive Design", "3D", "Motion Graphics"],
+            photo1: 'images/danseglio_photo1',
+            photo2: 'images/danseglio_photo2',
+            link: "http://sarahdanseglio.com/index.html"
+
+        }, ]
     },
 });
