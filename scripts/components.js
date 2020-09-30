f7d1e3a38e78bc5d4d3c38f60960d741557aca1a
Jin Jin Heipler <heipler.jj@gmail.com>
2020-09-30 15:40:58 -0400

    removed class name from root div. commented out grid styles

--- scripts/components.js
+++ scripts/components.js
@@ -2,30 +2,20 @@
 
 Vue.component('portfolio-slat', {
     props: ['pfp', 'major', 'name', 'blurb', 'chips', 'photo1', 'photo2'],
-    template: `<div class="portfolio>
-                <b-row>
-                    <b-col>
-                        <div class="pfp_holder">
-                            <b-img v-bind:src="pfp" alt="Profile Picture"></b-img>
-                        </div>
-                    </b-col>
-                    <b-col>
-                        <div class="info">
-                            <span class="major">{{ major }}</span>
-                            <h2 class="name">{{ name }}</h2>
-                            <p class="bio">{{ blurb }}</p>
-                        </div>
-                    </b-col>
-                    <b-col>
-                        <div class="photo-holder>
-                            <b-img thumbnail fluid v-bind:src="photo1" alt="First project image"</b-img>
-                        </div>
-                    </b-col>
-                    <b-col>
+    template: `<div>
+                    <div class="pfp_holder">
+                        <b-img class="pfp" v-bind:src="pfp" alt="Profile Picture"></b-img>
+                    </div>
+                    <div class="info">
+                        <span class="major">{{ major }}</span>
+                        <h2 class="name">{{ name }}</h2>
+                        <p class="bio">{{ blurb }}</p>
+                    </div>
+                    <div class="photo-holder>
+                        <b-img thumbnail fluid v-bind:src="photo1" alt="First project image"</b-img>
+                    </div>
                     <div class="photo-holder>
                         <b-img thumbnail fluid v-bind:src="photo2" alt="Second project image"</b-img>
                     </div>
-                </b-col>
-                </b-row>
             </div>`
 });
