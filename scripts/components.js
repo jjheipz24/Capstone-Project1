fb89a88c6758c5a47937f563441ce983a1058653
Jin Jin Heipler <heipler.jj@gmail.com>
2020-09-30 21:09:27 -0400

    initial styles for portfolio slat

--- scripts/components.js
+++ scripts/components.js
@@ -1,21 +1,27 @@
 //Template for components
 
 Vue.component('portfolio-slat', {
-    props: ['pfp', 'major', 'name', 'blurb', 'chips', 'photo1', 'photo2'],
+    props: ['pfp', 'major', 'name', 'blurb', 'chips', 'photo1', 'photo2', 'link'],
     template: `<div>
-                    <div class="pfp_holder">
-                        <b-img class="pfp" v-bind:src="pfp" alt="Profile Picture"></b-img>
-                    </div>
-                    <div class="info">
-                        <span class="major">{{ major }}</span>
-                        <h2 class="name">{{ name }}</h2>
-                        <p class="bio">{{ blurb }}</p>
-                    </div>
-                    <div class="photo-holder>
-                        <b-img thumbnail fluid v-bind:src="photo1" alt="First project image"</b-img>
-                    </div>
-                    <div class="photo-holder>
-                        <b-img thumbnail fluid v-bind:src="photo2" alt="Second project image"</b-img>
+                    <div class="portfolio-content">
+                        <div class="pfp-holder">
+                            <b-img class="pfp" v-bind:src="pfp" alt="Profile Picture"></b-img>
+                        </div>
+                        <div class="info">
+                            <span class="major">{{ major }}</span>
+                            <h2 class="name">{{ name }}</h2>
+                            <p class="bio">{{ blurb }}</p>
+                            <div v-for="chip in chips">
+                            <span class="chip">{{chip}}</span>
+                            </div>
+                        </div>
+                        <div class="photo-holder">
+                            <b-img thumbnail fluid v-bind:src="photo1" alt="First project image"></b-img>
+                        </div>
+                        <div class="photo-holder">
+                            <b-img thumbnail fluid v-bind:src="photo2" alt="Second project image"></b-img>
+                        </div>
+                        <a v-bind:href="link">View Portfolio</a>
                     </div>
             </div>`
 });
